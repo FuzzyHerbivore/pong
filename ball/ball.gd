d4fbc827e2ba3bf2678557ec48c7574773bae0eb
Christian Schneider <fuzzyherbivore@icloud.com>
2024-10-27 16:02:38 +0100

    Make ball start movement with random angle.

--- ball/ball.gd
+++ ball/ball.gd
@@ -2,12 +2,22 @@ extends RigidBody2D
 
 
 @export var initial_speed := 100.0
+@export_range(0, 120, 1) var start_angle_spread := 60
 
 var velocity: Vector2
 
 
 func _ready() -> void:
-	velocity = Vector2(100.0, 0.0)
+	var invert_direction := bool(randi_range(0, 1))
+
+	var starting_velocity := Vector2.RIGHT
+	if invert_direction:
+		starting_velocity *= -1
+
+	var start_angle_deviation_from_x_axis := float(start_angle_spread) / 2.0
+	var random_direction_degrees := randf_range(-start_angle_deviation_from_x_axis, start_angle_deviation_from_x_axis)
+	starting_velocity = starting_velocity.rotated(deg_to_rad(random_direction_degrees))
+	velocity = starting_velocity * initial_speed
 
 
 func _physics_process(delta: float) -> void:
