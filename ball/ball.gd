cb5cef54fb66bb1afe6a6d0b01e503207c1c9135
Christian Schneider <fuzzyherbivore@icloud.com>
2024-10-27 15:27:23 +0100

    Added player movement.

--- ball/ball.gd
+++ ball/ball.gd
@@ -1,11 +1,14 @@
 extends RigidBody2D
 
 
+@export var initial_speed := 100.0
+
 var velocity: Vector2
 
 
 func _ready() -> void:
 	velocity = Vector2(100.0, 0.0)
+
 
 func _physics_process(delta: float) -> void:
 	var collision := move_and_collide(velocity * delta)
--- player/player.gd
+++ player/player.gd
@@ -1,5 +1,19 @@
-class_name Player extends Node2D
+class_name Player extends CharacterBody2D
 
 
-func _ready() -> void:
-	pass
+@export var move_up_action: String
+@export var move_down_action: String
+@export_range(50.0, 200.0) var movement_speed: float = 100.0
+
+
+func _physics_process(_delta: float) -> void:
+	var input_vector := Vector2.ZERO
+
+	if Input.is_action_pressed(move_up_action):
+		input_vector.y = -1.0
+	if Input.is_action_pressed(move_down_action):
+		input_vector.y = 1.0
+
+	velocity = input_vector.normalized() * movement_speed
+
+	move_and_slide()
